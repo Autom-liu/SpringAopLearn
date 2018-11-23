10f3f4563c1d816e2f7473f0ff788d7bd9a5c091
Autom-liu <349576362@163.com>
2018-11-23 17:00:36 +0800

    spring Aop事务，编码式——在Service中使用事务模板

--- src/edu/scnu/service/UserServiceImpl.java
+++ src/edu/scnu/service/UserServiceImpl.java
@@ -10,16 +10,31 @@ import edu.scnu.dao.UserDao;
 public class UserServiceImpl implements UserService {
 	
 	private UserDao ud;
+	
+	private TransactionTemplate txtemplate;
 
 	@Override
 	public void transfer(Integer from, Integer to, Double money) {
-		ud.decreaseAccount(from, money);
+		
+		txtemplate.execute(new TransactionCallbackWithoutResult() {
+			
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus arg0) {
+				
+				ud.decreaseAccount(from, money);
 
-		ud.increaseAccount(to, money);
+				ud.increaseAccount(to, money);
+			}
+		});
+
 	}
 
 	public void setUd(UserDao ud) {
 		this.ud = ud;
+	}
+
+	public void setTxtemplate(TransactionTemplate txtemplate) {
+		this.txtemplate = txtemplate;
 	}
 
 }
