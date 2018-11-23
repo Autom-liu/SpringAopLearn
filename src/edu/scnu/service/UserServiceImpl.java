eee174d5ffc444f5f7b6c9944fec3d7fb9969971
Autom-liu <349576362@163.com>
2018-11-23 17:20:23 +0800

    spring Aop事务，配置式

--- src/edu/scnu/service/UserServiceImpl.java
+++ src/edu/scnu/service/UserServiceImpl.java
@@ -10,31 +10,16 @@ import edu.scnu.dao.UserDao;
 public class UserServiceImpl implements UserService {
 	
 	private UserDao ud;
-	
-	private TransactionTemplate txtemplate;
 
 	@Override
 	public void transfer(Integer from, Integer to, Double money) {
-		
-		txtemplate.execute(new TransactionCallbackWithoutResult() {
-			
-			@Override
-			protected void doInTransactionWithoutResult(TransactionStatus arg0) {
-				
-				ud.decreaseAccount(from, money);
-
-				ud.increaseAccount(to, money);
-			}
-		});
-
+		ud.decreaseAccount(from, money);
+	
+		ud.increaseAccount(to, money);
 	}
 
 	public void setUd(UserDao ud) {
 		this.ud = ud;
-	}
-
-	public void setTxtemplate(TransactionTemplate txtemplate) {
-		this.txtemplate = txtemplate;
 	}
 
 }
