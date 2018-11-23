76f13a7e8358233f46449cfbb58ac3de67973646
Autom-liu <349576362@163.com>
2018-11-23 16:52:53 +0800

    整合Junit和spring，编写测试类

---
+++ src/edu/scnu/test/TestJdbc.java
@@ -0,0 +1,56 @@
+﻿package edu.scnu.test;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import edu.scnu.bean.User;
+import edu.scnu.dao.UserDao;
+import edu.scnu.service.UserService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+public class TestJdbc {
+	@Resource(name="userDao")
+	private UserDao ud;
+	@Resource(name="userService")
+	private UserService us;
+	
+	@Test
+	public void testSave() {
+		User u = new User();
+		u.setUser_name("测试用户");
+		u.setUser_state('0');
+		u.setAccount(1000d);
+		ud.save(u);
+	}
+	
+	@Test
+	public void testUpdate() {
+		User u = new User();
+		u.setUser_name("测试用户2");
+		ud.update(u);
+	}
+	
+	@Test
+	public void testDelete() {
+		ud.delete(7);
+	}
+	
+	@Test
+	public void testFind() {
+		System.out.println(ud.getById(7));
+		System.out.println(ud.getAll());
+	}
+
+	public void setUd(UserDao ud) {
+		this.ud = ud;
+	}
+
+	public void setUs(UserService us) {
+		this.us = us;
+	}
+}
